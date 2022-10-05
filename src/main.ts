82e01fe6d3e28df3c3ca7ec4bf5ad2b86131bf9a
Arthur Bolsoni <arthurbolsoni@hotmail.com>
2022-10-05 14:00:42 -0300

    on

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import cors from '@fastify/cors';
 import fastifyCsrf from '@fastify/csrf-protection';
 import helmet from '@fastify/helmet';
 import { NestFactory } from '@nestjs/core';
@@ -10,11 +11,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true }),
+    new FastifyAdapter({ logger: true })
   );
+  
+  app.register(cors, {
+    origin: "*",
+  });
 
-  await app.register(helmet, { contentSecurityPolicy: false });
-  await app.register(fastifyCsrf);
-  await app.listen(3000);
+  // await app.register(helmet, { contentSecurityPolicy: false });
+  // await app.register(fastifyCsrf);
+  await app.listen(80);
 }
 bootstrap();
